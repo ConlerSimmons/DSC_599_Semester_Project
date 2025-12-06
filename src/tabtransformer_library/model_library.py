2ff30933148f59d0eee7ba4b1dafdb48632eb0d9
ConlerSimmons <conlersimmons137913@gmail.com>
2025-12-06 13:54:23 -0600

    getting rid of progress bar functions completely

--- src/tabtransformer_library/model_library.py
+++ src/tabtransformer_library/model_library.py
@@ -21,9 +21,7 @@ def build_tabtransformer_library(numeric_cols, categorical_cols, target_col="isF
     trainer_config = TrainerConfig(
         batch_size=1024,
         max_epochs=5,
-        accelerator="cpu",
-        enable_progress_bar=False,
-        callbacks=[],  # Disable RichProgressBar and any Lightning progress bars
+        accelerator="cpu"
     )
 
     optimizer_config = OptimizerConfig(
